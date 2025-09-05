0c2e5562028a20850df019ef53be728d31d7ea12
taejeong0416 <tj041600@naver.com>
2025-09-05 13:32:28 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# TypeScript 프로그래밍 연습
+
+## 준비사항
+
+- Node.js >= 22.x
+- VSCode >= 최신버전
+
+## 실행
---
+++ src/sum.js
@@ -0,0 +1,8 @@
+function sum(x, y) {
+    return x + y;
+}
+
+const rst1 = sum(1, 1);
+const rst2 = sum("Hello", [0, 1, 2]);
+console.log(rst1);
+console.log(rst2);
